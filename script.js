190dbf915aa4b56b224eeee914f6da9c7ed22346
Rambod Goshtasbi <rpg225@gmail.com>
2022-02-10 14:49:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,123 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.querySelectorAll(".jsColor"); // use to change color
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.querySelector("#jsSave");
+const clear = document.querySelector("#jsClear");
+
+const INITIAL_COLOR = "2c2c2c";
+const CANVAS_SIZE = '700';
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+
+ctx.fillStyle =' white'
+ctx.fillRect(0,0,CANVAS_SIZE, CANVAS_SIZE)
+ctx.stokeStyle = INITIAL_COLOR
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth = 2.5;
+
+
+let painting = false;
+let filling = false;
+
+function stopPainting() {
+  painting = false
+}
+
+function startPainting() {
+  painting = true;
+}
+
+function onMouseMove(event) {
+   const x = event.offsetX;
+   const y = event.offsetY;
+   if (!painting) {
+     ctx.beginPath()
+     ctx.moveTo(x, y)
+   } else {
+     ctx.lineTo(x, y);
+     ctx.stroke()
+   }
+}
+
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor
+
+  ctx.strokeStyle = color;
+  ctx.fillStyle = ctx.strokeStyle
+}
+
+function handleRangeChange(event) {
+  // console.log(event.target.value)
+    const size = event.target.value
+    ctx.lineWidth = size
+}
+
+function handleModeClick() {
+  if(filling === true) {
+    filling = false
+    mode.innerText = "Fill"
+  } else {
+    filling = true
+    mode.innerText = "Paint"
+  }
+}
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0,0, CANVAS_SIZE, CANVAS_SIZE)
+  }
+}
+
+function handleRightControl(event) {
+  event.preventDefault()
+}
+
+function handleSaveClick() {
+  const image = canvas.toDataURL("image/jped")
+
+  const link = document.createElement("a")
+  link.href = image;
+  link.download = "👉PaintJS[EXPORT]";
+  link.click()
+}
+
+function handleClear() {
+  ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  ctx.beginPath();
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting)
+  canvas.addEventListener("mouseleave", stopPainting)
+  canvas.addEventListener("click", handleCanvasClick)
+  canvas.addEventListener("contextmenu", handleRightControl)
+}
+
+// Changing colors
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
+
+// Brush Size
+
+if (range) {
+  range.addEventListener("input", handleRangeChange)
+}
+
+if(mode) {
+  mode.addEventListener("click", handleModeClick)
+}
+
+// Save Button
+if(saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick)
+}
+// Clean Button
+
+if(clear) {
+  clear.addEventListener("click", handleClear)
+}
